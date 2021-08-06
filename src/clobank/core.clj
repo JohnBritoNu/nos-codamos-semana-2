b0c383983c1669c1d3cdfefd51c1c77af75e0e48
mateus.vieira <mateus17v@gmail.com>
2021-08-06 09:42:15 -0300

    Update - refactoring code to recive data from db

--- src/clobank/core.clj
+++ src/clobank/core.clj
@@ -1,8 +1,12 @@
 (ns clobank.core
-  (:require [clobank.logic :as cb.logic]))
+  (:require [clobank.logic :as cb.logic]
+            [clobank.db :as cb.db]
+            [schema.core :as s]))
+
+(s/set-compile-fn-validation! true)
 
 (println "\nTransações por categoria: ")
-(cb.logic/transactions-by-category)
+(cb.logic/transactions-by-category (cb.db/clobank-database))
 
 (println "\nTransações: ")
-(cb.logic/list-transactions)
+(cb.logic/list-transactions (cb.db/clobank-database))
